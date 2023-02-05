a5ceb6698cf9ef1966c6026f91c4914801481a50
Bharath <karla.bharathnandan@gmail.com>
2023-02-05 04:02:54 -0500

    added

--- project/authfile.py
+++ project/authfile.py
@@ -26,11 +26,11 @@ def login_post():
     # take the user-supplied password, hash it, and compare it to the hashed password in the database
     if not user or not check_password_hash(user.password, password):
         flash('Please check your login details and try again.')
-        return redirect(url_for('auth.login')) # if the user doesn't exist or password is wrong, reload the page
+        return redirect(url_for('webpage/auth.login')) # if the user doesn't exist or password is wrong, reload the page
 
     # if the above check passes, then we know the user has the right credentials
     login_user(user, False)
-    return redirect(url_for('main.profile'))
+    return redirect(url_for('webpage/main.profile'))
 
 @auth.route('/signup')
 def signup():
@@ -59,13 +59,13 @@ def signup_post():
 
     if user: # if a user is found, we want to redirect back to signup page so user can try again
         flash('Email address already exists')
-        return redirect(url_for('auth.signup'))
+        return redirect(url_for('webpage/auth.signup'))
     
     user = User.query.filter_by(username=username).first() # if this returns a user, then the email already exists in database
 
     if user: # if a user is found, we want to redirect back to signup page so user can try again
         flash('Username address already exists')
-        return redirect(url_for('auth.signup'))
+        return redirect(url_for('webpage/auth.signup'))
 
     # create a new user with the form data. Hash the password so the plaintext version isn't saved.
     new_user = User(email=email, count=count, filename=filename, fname=fname, lname=lname, username=username, password=generate_password_hash(password, method='sha256'))
@@ -73,10 +73,10 @@ def signup_post():
     # add the new user to the database
     db.session.add(new_user)
     db.session.commit()
-    return redirect(url_for('auth.login'))
+    return redirect(url_for('webpage/auth.login'))
 
 @auth.route('/logout')
 @login_required
 def logout():
     logout_user()
-    return redirect(url_for('main.index'))
+    return redirect(url_for('webpage/main.index'))
