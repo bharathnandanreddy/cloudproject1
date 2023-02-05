7118a28501d75eee992784a1bd77a247c5606fc0
Bharath <karla.bharathnandan@gmail.com>
2023-02-05 10:09:54 -0500

    added

--- project/mainfile.py
+++ project/mainfile.py
@@ -13,5 +13,5 @@ def index():
 def profile():
     filename=current_user.filename
     if filename!="" and filename!=None:
-        filename='http://52.86.81.249/files/'+filename
+        filename='http://ec2-52-86-81-249.compute-1.amazonaws.com/files/'+filename
     return render_template('profile.html',count=current_user.count, filename=filename, fname=current_user.fname,lname=current_user.lname, email=current_user.email, loggedin=current_user.is_authenticated)
